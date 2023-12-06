d80b982732951e4981656aae4092003a60588ab7
Mohammad Hossein Amirpanahi <mhap75@yahoo.com>
2023-12-06 07:38:00 +0330

    finish

--- auth.config.ts
+++ auth.config.ts
@@ -1,20 +1,20 @@
-import type {NextAuthConfig} from 'next-auth';
+import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
-    pages: {
-        signIn: '/login',
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
     },
-    callbacks: {
-        authorized({auth, request: {nextUrl}}) {
-            const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
-            if (isOnDashboard) {
-                return isLoggedIn;
-            } else if (isLoggedIn) {
-                return Response.redirect(new URL("/dashboard", nextUrl))
-            }
-            return true
-        }
-    },
-    providers: []
-} satisfies NextAuthConfig
+  },
+  providers: [],
+} satisfies NextAuthConfig;
